47c608a4a9736a4567da93f51ffef7af2f9486cb
yash reddy <yashmunurreddy63@gmail.com>
2026-02-15 19:09:40 +0530

    modified the sendgrid settings

--- Pralay/settings.py
+++ Pralay/settings.py
@@ -147,12 +147,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = "apikey"
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Session Configuration
 SESSION_COOKIE_AGE = 86400  # 24 hours in seconds
