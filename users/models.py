d0b912ce9595b3326d7a10cd59509544802e226c
yash reddy <yashmunurreddy63@gmail.com>
2026-03-04 11:03:36 +0530

    forget to add profile_picture in customuser model

--- users/models.py
+++ users/models.py
@@ -24,6 +24,7 @@ class CustomUser(AbstractUser):
     
     # Contact information
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.FileField(upload_to='profile_pictures/', blank=True, null=True)
     
     # Role and authority information
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='user')
