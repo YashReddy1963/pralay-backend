95fc1704fd94608626b494c58aa6e0527b160e45
yash reddy <yashmunurreddy63@gmail.com>
2026-03-06 09:44:49 +0530

    Fix Cloudinary storage configuration

--- Pralay/settings.py
+++ Pralay/settings.py
@@ -134,9 +134,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media files (User uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
