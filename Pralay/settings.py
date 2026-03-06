7a1780fba3dbd257df41b030dd062dc72668f127
yash reddy <yashmunurreddy63@gmail.com>
2026-03-06 09:51:46 +0530

    Reverted the upgrade change

--- Pralay/settings.py
+++ Pralay/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
